fd68ade800200eb9f9b0c06983af1c8760555c0b
Arshdeep Singh <arshdeep932985@gmail.com>
2024-05-31 15:06:09 +0530

    completed logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,7 +139,29 @@ const loginUser=asyncHandler(async(req,res)=>{
 
 
 const logoutUser=asyncHandler(async(req,res)=>{
-    res.status(200)
+    await User.findByIdAndUpdate(
+       req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+    },
+    {
+        new:true
+    }
+    )
+
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User logged Out"))
 })
 
 export {
